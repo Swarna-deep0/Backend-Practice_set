c2db42c160146b05de6e370da5610f9b4c7821df
Swarna-deep0 <srj368920@gmail.com>
2025-08-29 13:38:18 +0530

    stream api

--- src/Stream_api.java
+++ src/Stream_api.java
@@ -1,9 +1,30 @@
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.*;
+class employee implements Comparable<employee>{
+    int id;
+    String name;
+    int salary;
+    String department;
 
+    employee( int id, String name, int salary, String department){
+        this.id =id;
+        this.name =name;
+        this.salary =salary;
+        this.department =department;
+    }
+
+    @Override
+    public int compareTo(employee other) {
+        return Integer.compare(this.salary, other.salary);
+    }
+
+    public String toString(){
+        return "[" + department  + " : $" + salary + " ]";
+    }
+}
 public class Stream_api {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(2,45,78,23,90,12,66);
+        List<Integer> numbers = Arrays.asList(2, 45, 78, 23, 90, 12, 66);
 
         // Finding the second largest
         Integer secondlargest = numbers.stream()
@@ -11,7 +32,7 @@ public class Stream_api {
                 .skip(1)
                 .findFirst()
                 .orElse(-1);
-        
+
         System.out.println("Second largest: " + secondlargest);
 
         // Finding the second minimum
@@ -21,5 +42,28 @@ public class Stream_api {
                 .findFirst()
                 .orElse(-1);
         System.out.println("Second minimum: " + secondMin);
+
+        List<employee> l1 = Arrays.asList(
+                new employee(1, "deep", 7000, "HR"),
+                new employee(2, "Khusi", 5000, "Manager"),
+                new employee(3, "animesh", 10000, "Manager"),
+                new employee(4, "anil", 3000, "HR"),
+                new employee(2, "Khusi", 5000, "IT"));
+        employee highestPaid = l1.stream()
+                .sorted(Comparator.reverseOrder())
+                .findFirst()
+                .orElse(null);
+        System.out.println("Highest paid: " + highestPaid);
+
+        // Highest salary from each department
+        Map<String, employee> highestByDept = l1.stream()
+                .collect(Collectors.toMap(
+                        emp -> emp.department,
+                        emp -> emp,
+                        (e1, e2) -> e1.salary > e2.salary ? e1 : e2
+                ));
+
+        highestByDept.forEach((dept, emp) ->
+                System.out.println(dept + ": " + emp));
     }
 }
